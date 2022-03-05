75ba39343e3212b9d23720bca06cfcbfa4df3c12
chubbykat <42463809+mewmix@users.noreply.github.com>
2022-03-04 19:17:18 -0800

    Add files via upload

---
+++ js/app/constants.js
@@ -0,0 +1,28 @@
+var FieldNames = {
+	ACCESS_TOKEN : "access_token",
+	REFRESH_TOKEN : "refresh_token",
+	ACCESS_TOKEN_TIMESTAMP : "access_token_timestamp",
+	ACCESS_TOKEN_ENDPOINT: "ACCESS_TOKEN_ENDPOINT",
+	REFRESH_TOKEN_ENDPOINT: "REFRESH_TOKEN_ENDPOINT",
+	HOSTED_START_PAGE_ADDR: "HOSTED_MAIN_PAGE_ADDRESS",
+	HOSTED_MAIN_PAGE_ADDR : "HOSTED_MAIN_PAGE_ADDR",
+	ENROLLED_COURSES: "ENROLLED_COURSES",
+	USER_ENDPOINT: "USER_ENDPOINT",
+	REDIRECT_URL: "REDIRECT_URL",
+	USER_ID: "user_id"
+};
+
+var Constants = {
+	THRESHOLD_BUFFER_TIME_MS  :  (12 * 60 * 60 * 1000) ,
+	APP_SECRET  :  "accff529-59ca-4a0a-96f6-499fd66c7b29",
+	APP_ID  :  "7761e521-0448-4bac-a572-9fa5b843e649",
+	PRIME_SERVER_ADDR  :  "https://captivateprime.adobe.com/",
+	MY_APP_HOSTED_SERVER_BASE_ADDR  :  "https://dev87927.service-now.com/",
+	SCOPE  :  "learner:read,learner:write",
+	OCODE_DATA  :  "state1",
+	SOURCE_FOLDER  :  "",
+	MAIN_PAGE  :  "oauthredirect.html",
+	START_PAGE : "index.html",
+	API_VERSION: "primeapi/v2/",
+	DEFAULT_LO_TYPE : "course"
+};
